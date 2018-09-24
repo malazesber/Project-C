864318257eceb5f4d949ce883731bf9d8f179131
Berkay Ilhan <0925207@hr.nl>
2018-09-24 10:14:55 +0200

    Navigatie menu werkt

---
+++ webtest/webtest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WishList()
+        {
+            ViewBag.Message = "THIS IS THE WISHLIST PAGE";
+
+            return View();
+        }
+
+        public ActionResult OrderStatus()
+        {
+            ViewBag.Message = "THIS IS THE ORDER STATUS PAGE";
+
+            return View();
+        }
+    }
+}
---
+++ webtest/webtest/Scripts/Test.js
@@ -0,0 +1,6 @@
+﻿$('.menu-toggle').click(function () {
+
+    $('.site-nav').toggleClass('site-nav--open', 500);
+    $(this).toggleClass('open');
+
+})
