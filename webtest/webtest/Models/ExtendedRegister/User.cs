db415b15fdd1045de3b1666e3f81ef90b4d31095
malazesber <31689460+malazesber@users.noreply.github.com>
2018-09-27 19:18:55 +0200

    not done register function

---
+++ webtest/webtest/Models/ExtendedRegister/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace webtest.Models
+{
+    public partial class User
+    {
+        
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert hier your first name")]
+        public string Name { get; set; }
+
+        [Display (Name = "Last Name")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert hier your Last Name")]
+        public string Surname { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert hier your email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+    }
+
+}
