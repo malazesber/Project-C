ccc7616bc717ac3ba57c02c983123394bc3cd0ed
Charline Walstra <0930515@hr.nl>
2018-09-30 20:58:18 +0200

    Error messages login page.

--- webtest/webtest/Models/ExtendedRegister/User.cs
+++ webtest/webtest/Models/ExtendedRegister/User.cs
@@ -15,31 +15,31 @@ namespace webtest.Models
     public class UserMetadata
     {
         [Display(Name = "First Name")]
-        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert hier your first name")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert your first name")]
         public string Name { get; set; }
 
         [Display (Name = "Last Name")]
-        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert hier your Last Name")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Please insert your surname")]
         public string Surname { get; set; }
 
         [Display(Name ="Email")]
-        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert hier your email")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert your email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert hier your Password")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert your Password")]
         [DataType(DataType.Password)]
         [MinLength(6,ErrorMessage ="Password must be at least 6 charachters")]
-        [RegularExpression("^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$", ErrorMessage ="Your Password must contain at least one capital letter one number and one small letter")]
+        [RegularExpression("^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$", ErrorMessage ="Your Password must contain at least one capital letter, one number and one small letter")]
         public string Password { get; set; }
 
         [Display(Name= "Confirm Password")]
         [DataType(DataType.Password)]
-        [Compare("Password",ErrorMessage ="Your Confirm Password does not match your Password ")]
+        [Compare("Password",ErrorMessage ="The passwords do not match")]
         public string ConfirmPassword { get; set; }
 
         [Display(Name="Phone Number")]
-        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert hier your Phone Number")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please insert your Phone Number")]
         public string Phone_number { get; set; }
     }
 
