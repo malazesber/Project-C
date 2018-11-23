2ebe4410fc9496b3e8d1e3e0926eb8b238d9fd79
malaz <39386359+malazEsper@users.noreply.github.com>
2018-11-23 23:10:14 +0100

    OrderHistory view+OrderDetails(Controller+view)

---
+++ webtest/webtest/webtest/Controllers/OrderDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webtest.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        // GET: OrderDetails
+        public ActionResult OrderDetails()
+        {
+            return View();
+        }
+    }
+}
